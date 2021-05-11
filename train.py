ba1700caab179736509f316e0e400c6c2d76b026
Paul Häusner <paul.haeusner@freenet.de>
2021-05-11 11:30:42 +0200

    fix some errors and clear outputs

--- train.py
+++ train.py
@@ -30,7 +30,8 @@ parser.add_argument('--size', type=int, default=256, help='size of the data crop
 parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
 parser.add_argument('--cuda', action='store_true', help='use GPU computation')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--n_cpu', type=int, default=2, help='number of cpu threads to use during batch generation')
+parser.add_argument('--log_interval', type=int, default=25, help='How often image examples should be logged to wandb')
 opt = parser.parse_args()
 print(opt)
 
@@ -116,11 +117,11 @@ for epoch in range(opt.epoch, opt.n_epochs):
         # GAN loss
         fake_B = netG_A2B(real_A)
         pred_fake = netD_B(fake_B)
-        loss_GAN_A2B = criterion_GAN(pred_fake, target_real)
+        loss_GAN_A2B = criterion_GAN(pred_fake.reshape(-1), target_real)
 
         fake_A = netG_B2A(real_B)
         pred_fake = netD_A(fake_A)
-        loss_GAN_B2A = criterion_GAN(pred_fake, target_real)
+        loss_GAN_B2A = criterion_GAN(pred_fake.reshape(-1), target_real)
 
         # Cycle loss
         recovered_A = netG_B2A(fake_B)
@@ -141,12 +142,12 @@ for epoch in range(opt.epoch, opt.n_epochs):
 
         # Real loss
         pred_real = netD_A(real_A)
-        loss_D_real = criterion_GAN(pred_real, target_real)
+        loss_D_real = criterion_GAN(pred_real.reshape(-1), target_real)
 
         # Fake loss
         fake_A = fake_A_buffer.push_and_pop(fake_A)
         pred_fake = netD_A(fake_A.detach())
-        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+        loss_D_fake = criterion_GAN(pred_fake.reshape(-1), target_fake)
 
         # Total loss
         loss_D_A = (loss_D_real + loss_D_fake)*0.5
@@ -160,12 +161,12 @@ for epoch in range(opt.epoch, opt.n_epochs):
 
         # Real loss
         pred_real = netD_B(real_B)
-        loss_D_real = criterion_GAN(pred_real, target_real)
+        loss_D_real = criterion_GAN(pred_real.reshape(-1), target_real)
         
         # Fake loss
         fake_B = fake_B_buffer.push_and_pop(fake_B)
         pred_fake = netD_B(fake_B.detach())
-        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+        loss_D_fake = criterion_GAN(pred_fake.reshape(-1), target_fake)
 
         # Total loss
         loss_D_B = (loss_D_real + loss_D_fake)*0.5
@@ -173,13 +174,13 @@ for epoch in range(opt.epoch, opt.n_epochs):
 
         optimizer_D_B.step()
         ###################################
+        # Progress report saved to wandb
+        wandb.log({'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A),
+                    'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B), 'epoch': epoch, 'iteration': i})
         # Only log every 25 steps
-        if i % 25 == 0:
-            # Progress report saved to wandb
-            wandb.log({'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A),
-                        'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B), 'epoch': epoch, 'iteration': i})
-            wandb.log({'img': [wandb.Image(real_A, caption="Real class A"), wandb.Image(fake_B, caption="Fake class B"),
-                            wandb.Image(real_B, caption="Real class B"), wandb.Image(fake_A, caption="Fake class A")]})
+        if i % opt.log_interval == 0:
+            wandb.log({'img': [wandb.Image(real_A, caption="Real class A"), wandb.Image(fake_B, caption="Fake class B"), 
+                               wandb.Image(real_B, caption="Real class B"), wandb.Image(fake_A, caption="Fake class A")]})
 
     # Update learning rates
     lr_scheduler_G.step()
