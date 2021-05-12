6c8e6e342a61ebd0539b93d93f598176e28499ee
Paul Häusner <paul.haeusner@freenet.de>
2021-05-12 10:43:32 +0200

    fix non rgb images

--- datasets.py
+++ datasets.py
@@ -4,6 +4,7 @@ import os
 
 from torch.utils.data import Dataset
 from PIL import Image
+import imageio
 import torchvision.transforms as transforms
 
 class ImageDataset(Dataset):
@@ -13,14 +14,20 @@ class ImageDataset(Dataset):
 
         self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
         self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+        
+    def l2rgb(self, image):
+        if image.mode != "RGB":
+            return image.convert("RGB")
+        else:
+            return image
 
     def __getitem__(self, index):
-        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        item_A = self.transform(self.l2rgb(Image.open(self.files_A[index % len(self.files_A)])))
 
         if self.unaligned:
-            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+            item_B = self.transform(self.l2rgb(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])))
         else:
-            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+            item_B = self.transform(self.l2rgb(Image.open(self.files_B[index % len(self.files_B)])))
 
         return {'A': item_A, 'B': item_B}
 
